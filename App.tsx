b427b23b5371b496767e768905a9f69f73027a84
loekTheDreamer <luke.carelsen@me.com>
2022-02-15 14:21:59 +0200

    ExConstants working with node symlink

--- App.tsx
+++ App.tsx
@@ -1,97 +1,97 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, {useRef, useState} from 'react';
+import {TouchableWithoutFeedback} from 'react-native';
+import {Canvas, useFrame} from '@react-three/fiber/native';
+// import { OrbitControls } from '@react-three/drei/native';
+import * as THREE from 'three';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+function Box(props: any) {
+  const mesh = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App = () => {
+  // console.log(active);
   return (
-    <SafeAreaView>
-      <View>
-        <Text>hello</Text>
-      </View>
-    </SafeAreaView>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={event => console.log(event)}
+      // onPress={(event: any) => console.log(event)}
+      onPointerOver={event => setHover(true)}
+      onPointerOut={event => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+export default function App() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[0, 0, 0]} />
+    </Canvas>
+  );
+}
 
-export default App;
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * Generated with the TypeScript template
+//  * https://github.com/react-native-community/react-native-template-typescript
+//  *
+//  * @format
+//  */
 
-// import React, {useRef, useState} from 'react';
-// import {TouchableWithoutFeedback} from 'react-native';
-// import {Canvas, useFrame} from '@react-three/fiber/native';
-// // import { OrbitControls } from '@react-three/drei/native';
-// import * as THREE from 'three';
+// import React from 'react';
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   useColorScheme,
+//   View,
+// } from 'react-native';
 
-// function Box(props: any) {
-//   const mesh = useRef<THREE.Mesh>(null!);
-//   const [hovered, setHover] = useState(false);
-//   const [active, setActive] = useState(false);
-//   useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
 
-//   // console.log(active);
+// const App = () => {
 //   return (
-//     <mesh
-//       {...props}
-//       ref={mesh}
-//       scale={active ? 1.5 : 1}
-//       onClick={event => console.log(event)}
-//       // onPress={(event: any) => console.log(event)}
-//       onPointerOver={event => setHover(true)}
-//       onPointerOut={event => setHover(false)}>
-//       <boxGeometry args={[1, 1, 1]} />
-//       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-//     </mesh>
+//     <SafeAreaView>
+//       <View>
+//         <Text>hello</Text>
+//       </View>
+//     </SafeAreaView>
 //   );
-// }
+// };
 
-// export default function App() {
-//   return (
-//     <Canvas>
-//       <ambientLight />
-//       <pointLight position={[10, 10, 10]} />
-//       <Box position={[0, 0, 0]} />
-//     </Canvas>
-//   );
-// }
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+
+// export default App;
